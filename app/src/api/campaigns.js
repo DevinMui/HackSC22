883e10bbc33ec49a8e64525d68f92ea00253c1fb
Devin Mui <devinwmui@gmail.com>
2022-02-05 15:16:55 -0800

    sry prettier

--- app/src/api/campaigns.js
+++ app/src/api/campaigns.js
@@ -1,20 +1,17 @@
 async function getCampaigns(type, id) {
-  switch(type) {
-    case 'highlights': 
-      return [] // List of popular + own
-    case 'subscribed': 
-      return [] // List of currently subscribed repos
-    default: // explore
-      return [] // List of popular repos
+  switch (type) {
+    case "highlights":
+      return []; // List of popular + own
+    case "subscribed":
+      return []; // List of currently subscribed repos
+    default:
+      // explore
+      return []; // List of popular repos
   }
 }
 
-async function getCampaign(id) {
+async function getCampaign(id) {}
 
-}
+async function createCampaign(repo, goal) {}
 
-async function createCampaign(repo, goal) {
-
-}
-
-export {getCampaigns, getCampaign, createCampaign}
+export { getCampaigns, getCampaign, createCampaign };
