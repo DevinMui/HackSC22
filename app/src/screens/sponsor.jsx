39817ea7be98d6d33b14af6f3f77d4963a048a85
Devin Mui <devinwmui@gmail.com>
2022-02-05 20:49:28 -0800

    change url

--- app/src/screens/sponsor.jsx
+++ app/src/screens/sponsor.jsx
@@ -27,7 +27,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: 'http://localhost:3000/dash',
+        return_url: process.env.REACT_APP_HOST,
       },
     });
 
