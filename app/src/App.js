b60e563582c746d76155ac2fa37b76f4bb9bb2b6
Devin Mui <devinwmui@gmail.com>
2022-02-05 23:22:34 -0800

    pay

--- app/src/App.js
+++ app/src/App.js
@@ -14,6 +14,7 @@ import Landing from './screens/landing';
 import Login from './screens/login';
 import Campaign from './screens/campaign';
 import Sponsor from './screens/sponsor';
+import Admin from './screens/admin';
 
 const App = () => (
   <AuthProvider>
@@ -45,6 +46,14 @@ const App = () => (
             element={
               <RequireAuth>
                 <Sponsor />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/campaigns/:owner/:repo/admin"
+            element={
+              <RequireAuth>
+                <Admin />
               </RequireAuth>
             }
           />
---
+++ app/src/screens/admin.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const Admin = () => {
+  const { owner, repo } = useParams();
+  const [infos, setInfos] = useState([]);
+
+  const pay = async () => {
+    const res = await fetch(
+      `/campaigns/${encodeURIComponent(`${owner}/${repo}`)}/payout`
+    );
+    const { info } = await res.json();
+    setInfos(info);
+  };
+
+  return (
+    <>
+      <h1>
+        {owner}/{repo}
+      </h1>
+      <button onClick={pay}>Payout</button>
+      {infos.length > 0 && <h3>Emails Sent</h3>}
+      {infos.map((info) => (
+        <p>
+          Email:{' '}
+          <a target="_blank" rel="noreferrer" href={`{info.url}`}>
+            {info.url}
+          </a>
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default Admin;
